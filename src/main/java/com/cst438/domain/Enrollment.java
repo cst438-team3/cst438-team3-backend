4289c542914d2d22d2a67f50a630f3d5f7c07769
PRINCESANCHEZ <princesanchez72@gmail.com>
2025-03-16 16:43:38 -0700

    Implement Enrollment Controller and JPA entities for enrollments
    Signed-off-by: PRINCESANCHEZ <princesanchez72@gmail.com>

--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -14,4 +14,42 @@ public class Enrollment {
     // create relationship between enrollment and user entities
     // create relationship between enrollment and section entities
     // add getter/setter methods
+    
+    private String grade;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User student;
+
+    @ManyToOne
+    @JoinColumn(name = "section_no")
+    private Section section;
+
+    public int getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public User getStudent() {
+        return student;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setStudent(User student) {
+        this.student = student;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
