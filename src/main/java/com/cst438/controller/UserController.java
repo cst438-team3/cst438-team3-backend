96d52a123f7ac07ed55572a89aebcc58ef62911c
PRINCESANCHEZ <princesanchez72@gmail.com>
2025-04-18 11:47:03 -0700

    Commit with security set up changes
    Signed-off-by: PRINCESANCHEZ <princesanchez72@gmail.com>

--- src/main/java/com/cst438/controller/UserController.java
+++ src/main/java/com/cst438/controller/UserController.java
@@ -5,14 +5,19 @@ import com.cst438.domain.UserRepository;
 import com.cst438.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
+/*
+ * CRUD apis for User entity
+ *     List all users,
+ *     post new user,
+ *     update user - only selected fields name, email, type
+ */
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
@@ -22,11 +27,9 @@ public class UserController {
 
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    /**
-     list all users
-     */
     @GetMapping("/users")
-    public List<UserDTO> findAllUsers() {
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
+    public List<UserDTO> findAllUsers( ) {
 
         List<User> users = userRepository.findAllByOrderByIdAsc();
         List<UserDTO> userDTO_list = new ArrayList<>();
@@ -36,20 +39,16 @@ public class UserController {
         return userDTO_list;
     }
 
-    /**
-     Admin creates a user
-     */
     @PostMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public UserDTO createUser(@RequestBody UserDTO userDTO) {
+
         User user = new User();
         user.setName(userDTO.name());
         user.setEmail(userDTO.email());
-
-        // create password and encrypt it
         String password = userDTO.name()+"2024";
         String enc_password = encoder.encode(password);
         user.setPassword(enc_password);
-
         user.setType(userDTO.type());
         if (!userDTO.type().equals("STUDENT") &&
                 !userDTO.type().equals("INSTRUCTOR") &&
@@ -61,11 +60,11 @@ public class UserController {
         return new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getType());
     }
 
-    /**
-     Admin update a user's name, email or type
-     */
     @PutMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+
+
         User user = userRepository.findById(userDTO.id()).orElse(null);
         if (user==null) {
             throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "user id not found");
@@ -83,16 +82,15 @@ public class UserController {
         return new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getType());
     }
 
-    /**
-     Admin delete a user
-     */
     @DeleteMapping("/users/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public void  updateUser(@PathVariable("id") int id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user!=null) {
-            userRepository.delete(user);
-        }
 
+       User user = userRepository.findById(id).orElse(null);
+        if (user==null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user id not found");
+        }
+        userRepository.delete(user);
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,18 @@
 insert into term (term_id, tyear, semester, add_date, add_deadline, drop_deadline, start_date, end_date) values
-(5, 2023, 'Spring', '2024-11-01', '2026-01-30', '2023-01-30', '2023-01-15', '2023-05-17'),
-(6, 2023, 'Fall',   '2023-05-01', '2026-08-30', '2023-08-30', '2023-08-15', '2023-12-17'),
-(7, 2024, 'Spring', '2023-11-01', '2026-01-30', '2024-01-30', '2024-01-15', '2024-05-17'),
-(8, 2024, 'Fall' ,  '2024-05-01', '2024-08-30', '2024-08-30', '2024-08-15', '2024-12-17'),
-(9, 2024, 'Fall', '2024-11-01', '2026-04-30', '2025-04-30', '2025-01-15', '2025-05-17'),
-(10, 2025, 'Fall',  '2025-05-01', '2026-08-30', '2025-08-30', '2025-08-15', '2025-12-17');
+(5, 2022, 'Spring', '2021-11-01', '2022-01-30', '2022-01-30', '2022-01-15', '2022-05-17'),
+(6, 2022, 'Fall',   '2022-05-01', '2022-08-30', '2022-08-30', '2022-08-15', '2022-12-17'),
+(7, 2023, 'Spring', '2022-11-01', '2023-01-30', '2023-01-30', '2023-01-15', '2023-05-17'),
+(8, 2023, 'Fall' ,  '2023-05-01', '2023-08-30', '2023-08-30', '2023-08-15', '2023-12-17'),
+(9, 2024, 'Spring', '2023-11-01', '2024-03-30', '2024-03-30', '2024-01-15', '2024-05-17'),
+(10, 2024, 'Fall',  '2024-05-01', '2024-08-30', '2024-08-30', '2024-08-15', '2024-12-17');
 
 insert into user_table (id, name, email, password, type) values
 (1, 'david wisneski', 'dwisneski@csumb.edu', '', 'INSTRUCTOR'),
 (2, 'joshua gross', 'jgross@csumb.edu', '', 'INSTRUCTOR'),
 (3, 'thomas edison', 'tedison@csumb.edu', '', 'STUDENT'),
 (4, 'fred brooks', 'fbrooks@csumb.edu', '', 'ADMIN'),
-(5, 'ronnie coleman', 'rcoleman@csumb.edu', '', 'STUDENT');
+(5, 'user', 'user@csumb.edu', '$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue','STUDENT'),
+(6, 'admin', 'admin@csumb.edu', '$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW' , 'ADMIN');
 
 insert into course values
 ('cst238', 'Introduction to Programming', 5),
@@ -39,11 +40,11 @@ insert into section(section_no, course_id, sec_id, term_id, building, room, time
 insert into enrollment (enrollment_id, grade, section_no, user_id ) values
 (1, 'A', 1, 3),
 (2, 'B', 8, 3),
-(3, null, 10, 3);
+(3,  null, 10, 3);
 
 insert into assignment (assignment_id, section_no, title, due_date) values
-( 1, 8, 'db homework 1', '2025-02-01'),
-( 2, 8, 'db homework 2', '2025-02-15');
+( 1, 8, 'db homework 1', '2024-02-01'),
+( 2, 8, 'db homework 2', '2024-02-15');
 
 insert into grade (grade_id, enrollment_id, assignment_id, score) values
 ( 1, 2, 1, 95);
